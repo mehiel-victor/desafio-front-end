f82e0e9737b3143f29729d63b0b5b215f17504b4
Victor Mehiel Rocha <victormehiel97@gmail.com>
2025-03-19 20:24:56 -0300

    feat: add header component and position propriely

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
-import "./App.css";
+import Header from "./components/Header";
 import EmployeeTable from "./components/common/EmployeeTable";
 
-function App() {
-  return <EmployeeTable />;
-}
+const App: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="bg-neutral-5 flex-1">
+        <div className="flex items-center justify-between p-8">
+          <h1 className="text-h1 font-heading font-medium text-neutral-1">
+            Funcionários
+          </h1>
+        </div>
+        <EmployeeTable />
+      </main>
+    </div>
+  );
+};
 
 export default App;
