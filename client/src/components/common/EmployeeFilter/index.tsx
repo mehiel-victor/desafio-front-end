1cede627c3abf285433e21232420f64519c261d7
Victor Mehiel Rocha <victormehiel97@gmail.com>
2025-03-20 20:13:02 -0300

    feat: add new magnifier icon and position it on employeefilter input

--- client/src/components/common/EmployeeFilter/index.tsx
+++ client/src/components/common/EmployeeFilter/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Magnifier from "../Magnifier";
 
 interface EmployeeFilterProps {
   onFilterChange: (filter: string) => void;
@@ -14,12 +15,15 @@ const EmployeeFilter: React.FC<EmployeeFilterProps> = ({ onFilterChange }) => {
   };
 
   return (
-    <div className="">
+    <div className="relative">
+      <div className="absolute right-3 top-7/12 transform -translate-y-1/2">
+        <Magnifier />
+      </div>
       <input
         type="text"
         value={filterText}
         onChange={handleChange}
-        className="px-4 py-3 w-full border-2 bg-neutral-6 hover:border-primary/40 focus-visible:border-primary focus:border-primary  border-neutral-3 rounded-lg focus:outline-none"
+        className="px-4 py-3 border-2 bg-neutral-6 hover:border-primary/40 focus-visible:border-primary focus:border-primary border-neutral-3 rounded-lg focus:outline-none"
         placeholder="Pesquisar"
       />
     </div>
